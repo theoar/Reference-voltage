ca4e4dc6a37d49c5ebe6b47c403966a930bd779a
theoar <m.drozdz@10g.pl>
2018-12-17 13:33:06 +0100

    Create actions.h

---
+++ actions.h
@@ -0,0 +1,58 @@
+/*
+ * actions.h
+ *
+ *  Created on: 21-11-2017
+ *      Author: theoar
+ */
+
+#ifndef ACTIONS_H_
+#define ACTIONS_H_
+
+
+//akcje wewnetrzne dzialajace na menu itd
+#ifndef IO_ACTION_POS_LEFT
+#define IO_ACTION_POS_LEFT 1
+#endif
+
+#ifndef IO_ACTION_POS_RIGHT
+#define IO_ACTION_POS_RIGHT 2
+#endif
+
+#ifndef IO_ACTION_VAL_LEFT
+#define IO_ACTION_VAL_LEFT 3
+#endif
+
+#ifndef IO_ACTION_VAL_RIGHT
+#define IO_ACTION_VAL_RIGHT 4
+#endif
+
+#ifndef IO_ACTION_ENTER
+#define IO_ACTION_ENTER 5
+#endif
+
+#ifndef IO_ACTION_ESCAPE
+#define IO_ACTION_ESCAPE 6
+#endif
+
+#ifndef ACTION_GO_UP
+#define ACTION_GO_UP 1
+#endif
+
+#ifndef ACTION_NEW_PWM_SETTINGS
+#define ACTION_NEW_PWM_SETTINGS 30
+#endif
+
+#ifndef ACTION_RESET_PWM_SETTINGS
+#define ACTION_RESET_PWM_SETTINGS 31
+#endif
+
+#ifndef ACTION_NEW_VOLTAGE
+#define ACTION_NEW_VOLTAGE 32
+#endif
+
+#ifndef ACTION_ABORT_SETTINGS
+#define ACTION_ABORT_SETTINGS 33
+#endif
+
+
+#endif /* ACTIONS_H_ */
