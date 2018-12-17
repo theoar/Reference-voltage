b6032a4e9c62df0da54d22b22d24e6dc95b9c601
theoar <m.drozdz@10g.pl>
2018-12-17 13:33:36 +0100

    Create functions.h

---
+++ functions.h
@@ -0,0 +1,17 @@
+/*
+ * functions.h
+ *
+ *  Created on: 21-11-2017
+ *      Author: theoar
+ */
+
+#ifndef FUNCTIONS_H_
+#define FUNCTIONS_H_
+
+#include <inttypes.h>
+
+void str_cpy(char *Where, const char *What);
+uint8_t str_len(const char *What);
+
+
+#endif /* FUNCTIONS_H_ */
